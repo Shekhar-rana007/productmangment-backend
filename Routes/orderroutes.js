2192964edeecf27b04a9f0b9dd9d4ec0c964b324
Shekhar-rana007 <ranashekhar007@gmail.com>
2025-03-19 20:37:25 +0530

    20:37

--- Routes/orderroutes.js
+++ Routes/orderroutes.js
@@ -3,7 +3,7 @@ const { verifyToken, verifyAdmin } = require("../Middleware/authmiddleware");
 const { createOrder, getUserOrders, getAllOrders, updateOrderStatus } = require("../Controller/orderController");
 
 const router = express.Router();
-
+// ?test
 router.post("/createorder", verifyToken, createOrder);
 router.get("/getUserOrders", verifyToken, getUserOrders);
 router.get("/all", verifyAdmin, getAllOrders); 
